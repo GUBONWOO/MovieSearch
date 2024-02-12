791f3df5d8730ac44cfe6a3572afca0305e1f60e
GUBONWOO <zcwxzsx@gmail.com>
2024-02-12 19:32:59 +0900

    components part2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import { Component } from './core/heropy';
+import FruitItem from './components/FruitItem';
 
 export default class App extends Component {
   constructor() {
     super({
       state: {
-        inputText: '',
+        fruits: [
+          { name: 'apple', price: 1000 },
+          { name: 'banana', price: 2000 },
+          { name: 'orange', price: 3000 },
+        ],
       },
     });
   }
   render() {
-    this.el.classList.add('search');
     this.el.innerHTML = /* html */ `
-    <input />
-    <button>Click</button>
+  <h1>Fruits</h1>
+  <ul>
+
+  </ul>  
     `;
-    const inputEl = this.el.querySelector('input');
-    inputEl.addEventListener('input', () => {
-      this.state.inputText = inputEl.value;
-    });
-    const buttonEl = this.el.querySelector('button');
-    buttonEl.addEventListener('click', () => {
-      console.log(this.state.inputText);
-    });
+    const ulEl = this.el.querySelector('ul');
+
+    ulEl.append(
+      ...this.state.fruits
+        .filter((fruit) => fruit.price < 3000)
+        .map(
+          (fruit) =>
+            new FruitItem({
+              props: {
+                name: fruit.name,
+                price: fruit.price,
+              },
+            }).el
+        )
+    );
   }
 }
---
+++ src/components/FruitItem.js
@@ -0,0 +1,13 @@
+import { Component } from '../core/heropy';
+
+export default class FruitItem extends Component {
+  constructor(payload) {
+    super({
+      tagName: 'li',
+      props: payload.props,
+    });
+  }
+  render() {
+    this.el.textContent = this.props.name;
+  }
+}
