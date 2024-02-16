a86bd4e5c66d8116df5430ede4cfb402f4ed9c7a
GUBONWOO <zcwxzsx@gmail.com>
2024-02-16 11:44:32 +0900

    error control

--- src/store/movie.js
+++ src/store/movie.js
@@ -3,19 +3,35 @@ import { Store } from '../core/heropy.js';
 const store = new Store({
   searchText: '',
   page: 1,
+  pageMax: 1,
   movies: [],
+  loading: false,
+  message: 'Search for the movie title',
 });
 
 export default store;
 
 export const searchMovies = async (page) => {
+  store.state.loading = true;
+  store.state.page = page;
   if (page === 1) {
-    store.state.page = 1;
     store.state.movies = [];
+    store.state.message = '';
   }
-  const res = await fetch(
-    `https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
-  );
-  const { Search } = await res.json();
-  store.state.movies = [...store.state.movies, ...Search];
+  try {
+    const res = await fetch(
+      `https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
+    );
+    const { Search, totalResults, Response, Error } = await res.json();
+    if (Response === 'True') {
+      store.state.movies = [...store.state.movies, ...Search];
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+    } else {
+      store.state.message = Error;
+    }
+  } catch (error) {
+    console.log('search error');
+  } finally {
+    store.state.loading = false;
+  }
 };
